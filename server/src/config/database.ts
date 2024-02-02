140de6c7c54063500c6fa6e834c4119f3c4874d3
henrix494 <henrix494@gmail.com>
2024-02-02 17:12:42 +0200

    first commit

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,15 +1,15 @@
 // src/config/database.ts
 
 import { Sequelize } from "sequelize";
+import * as tedious from "tedious";
 const sequelize = new Sequelize({
   host: "work-flow.database.windows.net",
   dialect: "mssql",
   username: "natan494",
   password: "134679852Aaa!",
   database: "Work",
-  define: {
-    timestamps: false,
-  },
+  dialectModule: tedious,
+
   dialectOptions: {
     encrypt: true,
   },
