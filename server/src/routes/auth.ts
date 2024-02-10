f0bae2a73d870dbc8c405a9d6befef3be65b7d3e
henrix494 <henrix494@gmail.com>
2024-02-10 22:53:33 +0200

    added login page

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -32,6 +32,8 @@ router.post("/login", async (req, res, next) => {
         res.cookie("jwt", token, {
           httpOnly: false,
           maxAge: maxAge * 1000,
+          sameSite: "none",
+
           secure: true,
         });
         res.status(200).json({ user });
