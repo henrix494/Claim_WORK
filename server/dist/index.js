d764d19df9f73b4746ab5aefffe625745bbdff7a
henrix494 <henrix494@gmail.com>
2024-02-10 23:09:19 +0200

    test

--- server/dist/index.js
+++ server/dist/index.js
@@ -17,6 +17,7 @@ const CreateUsers_1 = __importDefault(require("./routes/CreateUsers"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const corsOptions = {
     allowedHeaders: [
@@ -34,7 +35,6 @@ const corsOptions = {
     exposedHeaders: ["set-cookie"],
 };
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
