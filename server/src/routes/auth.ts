5f0ea90d1be622ee119a3d9d546e8ca41f460b58
henrix494 <henrix494@gmail.com>
2024-02-11 01:04:40 +0200

    added login p

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -29,12 +29,16 @@ router.post("/login", async (req, res, next) => {
       );
       if (match) {
         const token = createToken(user.id);
-        res.cookie("jwt", token, {
-          maxAge: maxAge * 1000,
-          sameSite: "none",
-          secure: true,
-          domain: ".vercel.app",
-        });
+        // res.cookie("jwt", token, {
+        //   maxAge: maxAge * 1000,
+        //   sameSite: "none",
+        //   secure: true,
+        //   domain: ".vercel.app",
+        // });
+        res.setHeader(
+          "Set-Cookie",
+          `jwt=${token}; HttpOnly; Max-Age=${maxAge}; SameSite=None; Secure`
+        );
 
         // res.status(200).json({ user });
       } else {
