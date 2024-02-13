3d2aa39236de41c99372fa28e9baf30747aa81d2
henrix494 <henrix494@gmail.com>
2024-02-13 17:07:36 +0200

    added cors option

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,12 +5,7 @@ import * as tedious from "tedious";
 const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   dialect: "mssql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
