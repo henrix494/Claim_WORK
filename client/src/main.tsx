e8d7cc89a113d152b55c00cf060f8a0d245b7b2a
henrix494 <henrix494@gmail.com>
2024-02-19 22:39:38 +0200

    added analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
