41d429dc1262456a9b5bc3d20e34e76c7478c9a5
henrix494 <henrix494@gmail.com>
2024-02-20 09:28:24 +0200

    added batter support for dev ENV

--- server/src/config/configProd.ts
+++ server/src/config/configProd.ts
@@ -4,7 +4,7 @@ export const ProdConfig = {
   host: process.env.DB_HOST,
   dialect: "mssql",
 
-  username: process.env.USERNAME,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
 
   database: process.env.DB_DATABASE,
